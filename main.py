8f4267e3b20c9eb2eb5f8c9527349bfe4b5db0c9
Lanzoor <166265896+Lanzoor@users.noreply.github.com>
2024-04-07 17:23:08 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@
 import random, time, timeit
 from datetime import datetime
 from functions import *
-
 # Declaration of basic variables that contains the help for LCP
 
 LCPCurrentVersion = "Beta 1.0"
@@ -54,34 +53,34 @@ while True:
     current_time = datetime.now()
     formatted_date = f"{current_time.strftime("%B")} {str(ordinal(int(current_time.strftime("%d"))))}{current_time.strftime(", %Y")}"
     formatted_time = current_time.strftime("%I:%M:%S %p")
-    if LCPInput == "?exit":
-        print("Exiting LCP...")
-        time.sleep(0.5)
-        print("You exitted the LCP!")
-        break
-    elif LCPInput == "?help":
-        print(LCPHelp)
-    elif LCPInput == "?info":
-        print(LCPInfo)
-    elif LCPInput == "?updatelog":
-        print(LCPUpdateLog)
-    elif LCPInput in ("?rps", "?rockpaperscissors"):
-        rps()
-    elif LCPInput in ("?golt", "?greaterorlowerthan"):
-        golt()
-    elif LCPInput == "?date":
-        print(f"Your current date is {formatted_date}!")
-    elif LCPInput == "?time":
-        print(f"Your current time is {formatted_time}!")
-    elif LCPInput == "?datetime":
-        print(f"Your date and time are {formatted_time} {formatted_date}!")
-    elif LCPInput == "?ping":
-        latency = timeit.timeit("print('Pong!')", number=1)
-        print(f"Latency of the command input was {latency}s!")
-    elif LCPInput == "?randint":
-        x = tryIntInput("Choose your minimum number!\n>>> ")
-        y = tryIntInput("Choose your maximum number!\n>>> ")
-        print(f"My random choice between {x} and {y} is {random.randint(x, y)}!")
-    else:
-        print(f"\"{LCPInput}\" is not a valid command, try something else!")
-    # FIXME: Fix this fucking if-elif-else statement because it's hard to read
+    match LCPInput:
+        case "?exit":
+            print("Exiting LCP...")
+            time.sleep(0.5)
+            print("You exitted the LCP!")
+            break
+        case "?help":
+            print(LCPHelp)
+        case "?info":
+            print(LCPInfo)
+        case "?updatelog":
+            print(LCPUpdateLog)
+        case ("?rps" | "?rockpaperscissors"):
+            rps()
+        case ("?golt" | "?greaterorlowerthan"):
+            golt()
+        case "?date":
+            print(f"Your current date is {formatted_date}!")
+        case "?time":
+            print(f"Your current time is {formatted_time}!")
+        case "?datetime":
+            print(f"Your date and time are {formatted_time} {formatted_date}!")
+        case "?ping":
+            latency = timeit.timeit("print('Pong!')", number = 1)
+            print(f"Average latency of 100 command inputs were {latency}s!")
+        case "?randint":
+            x = tryIntInput("Choose your minimum number!\n>>> ")
+            y = tryIntInput("Choose your maximum number!\n>>> ")
+            print(f"My random choice between {x} and {y} is {random.randint(x, y)}!")
+        case _:
+            print(f"\"{LCPInput}\" is not a valid command, try something else!")
