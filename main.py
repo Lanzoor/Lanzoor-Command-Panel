0d1ae86264d8fcb2d4f83e01d230620d2236a721
Lanzoor <166265896+Lanzoor@users.noreply.github.com>
2024-04-07 14:10:49 +0900

    Update and rename LCP.py to main.py
    Seperated the functions to a different file

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 import random, time
 from datetime import datetime
+from functions import *
 
 # Declaration of basic variables that contains the help for LCP
 
@@ -39,70 +40,6 @@ Alpha v1.0.3: Added date and time commands.
 Alpha v1.0.6: Little Changes, a combined version of Alpha v1.0.4 and v1.0.5.'''
 
 print(LCPStartHelp)
-
-# Function declarations
-
-def tryGoltInput(prompt: str) -> int:
-    while True:
-        try:
-            return int(input(prompt))
-        except ValueError:
-            print("You dumdum, that is not an integer!")
-            
-def golt():
-    number = random.randint(0, 100)
-    attempts = 0
-    print(
-        '''Let's play the \"Greater or lower than\" game!
-        The rules are simple, I pick a random number between 1 and 100.
-        If you make a guess, I'll tell you whenever your guess is greater than my guess, 
-        or your guess is lower than my guess until you guess the value! Let's get started.''',
-    )
-
-    while True:
-        inputValue = tryGoltInput("Enter your guess!\n>>> ")
-        if inputValue > number:
-            if inputValue <= 100:
-                print(f"The number is lower than {inputValue}, try again!")
-            attempts += 1
-        elif inputValue < number:
-            print(f"The number is greater than {inputValue}, try again!")
-            attempts += 1
-        elif inputValue == number:
-            print(f"You've guessed it right! The answer was {number}, and you used {attempts} attempts in this game.")
-        else:
-            print("You dumdum, this number is out of the value!")
-            attempts += 1
-        break
-    
-def rps():
-    UserChoice = input("Choose one! Rock, Paper, or Scissors!\n>>> ")
-    CPUChoice = random.choice(["Rock", "Paper", "Scissors"])
-    UserChoice = UserChoice.capitalize().replace(" ","")
-    RpsChoice = ["Rock", "Paper", "Scissors"]
-    
-    while UserChoice not in RpsChoice:
-        UserChoice = input("You dumdum, that is not a valid choice! Try again.\n>>> ")
-    
-    if CPUChoice == UserChoice:
-        WinOrLose = "It's a draw!"
-    else:
-        WinningConditions = [
-            ("rock", "scissors"),
-            ("scissors", "paper"),
-            ("paper", "rock")
-        ]
-
-        if (UserChoice, CPUChoice) in WinningConditions:
-            WinOrLose = "You won!"
-        else:
-            WinOrLose = "I won!"
-    print(f"You picked {UserChoice}, I pick {CPUChoice}! {WinOrLose}")
-        
-def ordinal(n: int) -> str:
-    return str(n) + ("th" if 11 <= (n % 100) <= 13 else (['th', 'st', 'nd', 'rd', 'th'][min(n % 10, 4)]))
-
-# Command Inputs
 
 while True:
     LCPInput = input(">>> ").replace(" ", "").lower()
