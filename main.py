55a433c1a00bdc440f8e6f18260d41483c2db0c7
Lanzoor <166265896+Lanzoor@users.noreply.github.com>
2024-04-21 09:28:07 +0900

    Update main.py

--- main.py
+++ main.py
@@ -28,6 +28,7 @@ Welcome to LCP command help! Here are the list of all commands that you can use.
 ?randint: Pick a random value between the maximum and minimum value that you input.
 ?randkey: Generate a random key based on the length.
 ?flipacoin: Flip a coin.
+?points: Check how many points you currently have.
 '''
 
 Info = f'''
