d1d29e21017bd78c77327d9cb6649657be800091
Lanzoor <166265896+Lanzoor@users.noreply.github.com>
2024-05-07 15:21:59 +0900

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,16 +1,17 @@
-import random
+import random, sys
 from datetime import datetime
+from time import sleep
 
 def tryIntInput(prompt: str) -> int:
     while True:
         try:
             return int(input(prompt))
         except:
-            print("You dumdum, that is not an integer!")
+            printAnimation("You dumdum, that is not an integer!")
 def golt(points: int):
     number = random.randint(0, 100)
     attempts = 0
-    print(
+    printAnimation(
         '''
 Let's play the \"Greater or lower than\" game!
 The rules are simple, I pick a random number between 1 and 100.
@@ -23,26 +24,30 @@ or your guess is lower than my guess until you guess the value! Let's get starte
         inputValue = tryIntInput("Enter your guess!\n  >>> ")
         if inputValue > number:
             if inputValue <= 100:
-                print(f"The number is lower than {inputValue}, try again!")
+                printAnimation(f"The number is lower than {inputValue}, try again!")
             else:
-                print("You dumdum, this number is out of the value!")
+                printAnimation("You dumdum, that value is out of the range! Try a value that is under or equal to 100.")
             attempts += 1
         elif inputValue < number:
-            print(f"The number is greater than {inputValue}, try again!")
+            if inputValue >= 1:
+                printAnimation(f"The number is greater than {inputValue}, try again!")
+            else:
+                printAnimation("You dumdum, that value is under the range! Try a value that is greater or equal to 1!")
             attempts += 1
         elif inputValue == number:
             attempts += 1
-            print(f"You've guessed it right! The answer was {number}, and you used {attempts} attempts in this game. You gained 5 points!")
+            printAnimation(f"You've guessed it right! The answer was {number}, and you used {attempts} attempts in this game. You gained 5 points!")
             points += 5
             return points
-            break
     
 def rps(points: int):
     UserChoice = input("Choose one! Rock, Paper, or Scissors!\n  >>> ").capitalize().replace(" ","")
-    CPUChoice = random.choice(["Rock", "Paper", "Scissors"])
     RpsChoice = ["Rock", "Paper", "Scissors"]
+    CPUChoice = random.choice(RpsChoice)
+    
     while UserChoice not in RpsChoice:
         UserChoice = input("You dumdum, that is not a valid choice! Try again.\n  >>> ")
+    
     if CPUChoice == UserChoice:
         WinOrLose = "It's a draw!"
         addingPoints = 2
@@ -59,7 +64,7 @@ def rps(points: int):
             WinOrLose = "I won!"
             addingPoints = 1
     points += addingPoints
-    print(f"You picked {UserChoice}, I pick {CPUChoice}! {WinOrLose} You gained {addingPoints} points in this game.")
+    printAnimation(f"You picked {UserChoice}, I pick {CPUChoice}! {WinOrLose} You gained {addingPoints} points in this game.")
     return points
 
 def ordinal(n: int) -> str:
@@ -68,3 +73,10 @@ def ordinal(n: int) -> str:
 
 def isLeapYear(year: int = int(datetime.now().strftime("%Y"))) -> bool:
     return year % 4 == 0 and year % 100 != 0 or year % 400 == 0
+
+def printAnimation(message: str, delay: float | int = 0.005) -> None:
+    for char in message:
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        sleep(delay)
+    print()
